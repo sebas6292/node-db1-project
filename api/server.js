69c624af0cff0488edc97345f31a9a41339b7d79
Sebastian <sebas6292@gmail.com>
2021-11-01 20:33:16 -0700

    endpoints are working

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router')
+
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter)
 
 server.get('/', (req, res) => {
     res.status(404).json({
